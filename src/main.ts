af7e589e3fefdc88d10a2cb1f0d5a768d34fb13d
JayadithyaCosta <jayadithya18@gmail.com>
2024-08-08 02:25:19 +0530

    added custom cors origins

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,18 @@ dotenv.config(); // Load environment variables
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api'); // Set a global prefix for API routes
+
+  // Enable CORS
+  app.enableCors({
+    origin: [
+      'http://127.0.0.1:5500',
+      'http://localhost:3001',
+      'http://localhost:3000',
+    ], // Allow only these origins
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true, // If you need to include credentials like cookies
+  });
+
   await app.listen(3000);
 }
 bootstrap();
